66709ef960408155b1b47ea3158ee8089705e8ee
Junior Bernard <junior.bernard@opendigitaleducation.com>
2026-02-25 09:34:49 +0100

    chore: #SRE-5075, reset version after extract

---
+++ Jenkinsfile
@@ -0,0 +1,54 @@
+#!/usr/bin/env groovy
+
+pipeline {
+  agent any
+
+  stages {
+    stage("Initialization") {
+      when {
+        environment name: 'RENAME_BUILDS', value: 'true'
+      }
+      steps {
+        script {
+          sh './build.sh $BUILD_SH_EXTRA_PARAM init'
+          def version = sh(returnStdout: true, script: 'docker run -it --rm -u 1000:1000 --env MAVEN_CONFIG=/var/maven/.m2 -w /usr/src/maven -v ./:/usr/src/maven -v ~/.m2:/var/maven/.m2  opendigitaleducation/mvn-java8-node20:latest mvn -Duser.home=/var/maven help:evaluate -Dexpression=project.version -DforceStdout -q')
+          buildName "${env.GIT_BRANCH.replace("origin/", "")}@${version}"
+        }
+      }
+    }
+    stage('Build') {
+      steps {
+        checkout scm
+        sh './build.sh $BUILD_SH_EXTRA_PARAM init clean install'
+      }
+    }
+    stage('Test') {
+      steps {
+        script {
+          sh 'sleep 6'
+/*            try {
+            sh './build.sh $BUILD_SH_EXTRA_PARAM test'
+          } catch (err) {
+          }*/
+        }
+      }
+    }
+    stage('Publish') {
+      steps {
+        sh "DRY_RUN=false ./build.sh \$BUILD_SH_EXTRA_PARAM publish"
+      }
+    }
+
+    stage('Build image') {
+          steps {
+              sh 'edifice image'
+        }
+    }
+  }
+  post {
+    cleanup {
+      sh 'docker compose down'
+    }
+  }
+}
+
--- build.sh
+++ build.sh
@@ -88,6 +88,25 @@ echo "======================"
 init() {
   me=`id -u`:`id -g`
   echo "DEFAULT_DOCKER_USER=$me" > .env
+
+    # If CLI_VERSION is empty set $cli_version to latest
+  	if [ -z "$CLI_VERSION" ]; then
+  		CLI_VERSION="latest"
+  	fi
+  	# Create a build.compose.yaml file from following template
+  	cat <<EOF > build.compose.yaml
+services:
+  edifice-cli:
+    image: opendigitaleducation/edifice-cli:$CLI_VERSION
+    user: "$DEFAULT_DOCKER_USER"
+EOF
+  	# Copy /root/edifice from edifice-cli container to host machine
+  	docker compose -f build.compose.yaml create edifice-cli
+  	docker compose -f build.compose.yaml cp edifice-cli:/root/edifice ./edifice
+  	docker compose -f build.compose.yaml rm -fsv edifice-cli
+  	rm -f build.compose.yaml
+  	chmod +x edifice
+  	./edifice version
 }
 
 clean () {
@@ -98,155 +117,17 @@ clean () {
   fi
 }
 
-buildFrontend () {
-  # --- Build angularJS-based frontends
-  if [ "$MODULE" = "" ] || [ ! "$MODULE" = "admin" ] && [ ! -e ./"$MODULE"/frontend ]; then
-    #try jenkins branch name => then local git branch name => then jenkins params
-    echo "[buildNode] Get branch name from jenkins env..."
-    BRANCH_NAME=`echo $GIT_BRANCH | sed -e "s|origin/||g"`
-    if [ "$BRANCH_NAME" = "" ]; then
-      echo "[buildNode] Get branch name from git..."
-      BRANCH_NAME=`git branch | sed -n -e "s/^\* \(.*\)/\1/p"`
-    fi
-    if [ ! -z "$FRONT_TAG" ]; then
-      echo "[buildNode] Get tag name from jenkins param... $FRONT_TAG"
-      BRANCH_NAME="$FRONT_TAG"
-    fi
-    if [ "$BRANCH_NAME" = "" ]; then
-      echo "[buildNode] Branch name should not be empty!"
-      exit -1
-    fi
-
-    if [ "$BRANCH_NAME" = 'master' ] || [ "$BRANCH_NAME" = 'fix' ]  || [ "$BRANCH_NAME" = 'release' ]; then
-        echo "[buildNode] Use entcore version from package.json ($BRANCH_NAME)"
-        case `uname -s` in
-          MINGW*)
-            docker compose run --rm -u "$USER_UID:$GROUP_GID" $CI_OPTION node sh -c "npm install --no-bin-links --legacy-peer-deps && npm update --legacy-peer-deps entcore && node_modules/gulp/bin/gulp.js build $NODE_OPTION"
-            ;;
-          *)
-            docker compose run --rm -u "$USER_UID:$GROUP_GID" $CI_OPTION node sh -c "npm install --legacy-peer-deps && npm update --legacy-peer-deps entcore && node_modules/gulp/bin/gulp.js build $NODE_OPTION --springboard=/home/node/$SPRINGBOARD"
-        esac
-    else
-        echo "[buildNode] Use entcore tag $BRANCH_NAME"
-        docker compose run --rm -u "$USER_UID:$GROUP_GID" $CI_OPTION node sh -c "npm rm --no-save entcore ode-ts-client ode-ngjs-front && npm install --no-save entcore@$BRANCH_NAME ode-ts-client@$BRANCH_NAME ode-ngjs-front@$BRANCH_NAME"
-        case `uname -s` in
-          MINGW*)
-            docker compose run --rm -T -u "$USER_UID:$GROUP_GID" $CI_OPTION node sh -c "npm install --no-bin-links --legacy-peer-deps && node_modules/gulp/bin/gulp.js build $NODE_OPTION"
-            ;;
-          *)
-            docker compose run --rm -T -u "$USER_UID:$GROUP_GID" $CI_OPTION node sh -c "npm install --legacy-peer-deps && node_modules/gulp/bin/gulp.js build $NODE_OPTION --springboard=/home/node/$SPRINGBOARD"
-        esac
-    fi
-  fi
-
-  # --- Build react-based frontends
-  local modules
-  if [ "$MODULE" = "" ]; then
-    modules=($(ls -d */ | cut -f1 -d'/'))
-  else 
-    modules=($MODULE)
-  fi
-  
-  for module in "${modules[@]}"; do
-    if [ -e ./"$module"/frontend ]; then
-      echo -e "[Build React] Build react frontend for module $module"
-      cd ./"$module"/frontend
-      if [ "$NO_DOCKER" = "true" ] ; then
-        ./build.sh --no-docker clean init build
-      else 
-        ./build.sh clean init build
-      fi
-      if [ $? -ne 0 ]; then
-        echo "Error while building React frontend for module $module"
-        exit 1
-      fi
-
-      # Create directory structure and copy frontend build files to backend
-      rm -rf ../backend/src/main/resources/public/*.js
-      rm -rf ../backend/src/main/resources/public/*.css
-      cp -R ./dist/* ../backend/src/main/resources/
-
-      # Create view directory and copy HTML files
-      mkdir -p ../backend/src/main/resources/view
-      mv ../backend/src/main/resources/*.html ../backend/src/main/resources/view
-
-      # Clean up
-      rm -rf ./dist
-      cd ../..
-    fi
-  done
-
-  # --- Build angular-based frontends
-  if [ "$MODULE" = "" ] || [ "$MODULE" = "admin" ]; then
-    case `uname -s` in
-      MINGW*)
-        docker compose run --rm -u "$USER_UID:$GROUP_GID" $CI_OPTION node16 sh -c "npm install --no-bin-links && npm rm --no-save ngx-ode-core ngx-ode-sijil ngx-ode-ui && npm install --no-save ngx-ode-core@$BRANCH_NAME ngx-ode-sijil@$BRANCH_NAME ngx-ode-ui@$BRANCH_NAME && npm run build-docker-prod"
-        ;;
-      *)
-        docker compose run --rm -u "$USER_UID:$GROUP_GID" $CI_OPTION node16 sh -c "npm install && npm rm --no-save ngx-ode-core ngx-ode-sijil ngx-ode-ui && npm install --no-save ngx-ode-core@$BRANCH_NAME ngx-ode-sijil@$BRANCH_NAME ngx-ode-ui@$BRANCH_NAME && npm run build-docker-prod"
-    esac
-  fi
-}
-
-buildBackend () {
-  if [ "$NO_DOCKER" = "true" ] ; then
-    mvn $MVN_OPTS install -DskipTests
-  else
-    docker compose run --rm $USER_OPTION maven mvn $MVN_OPTS install -DskipTests
-  fi
-}
-
 install () {
   docker compose run $CI_OPTION --rm maven mvn $MVN_OPTS clean install -DskipTests
-  cd broker-parent/broker-client/quarkus
-  ./build.sh install
-  cd -
+  buildBroker
 }
 
 buildBroker () {
-  edifice install --all=false --clients=false --client-nest=true
-  #./build.sh install
-  #cd -
+  ./edifice install --all=false --clients=true --service=false
 }
 
 test () {
-  if [ -z "$JAVA_8_HOME" ]
-  then
-    mvn test
-  else
-    JAVA_HOME=$JAVA_8_HOME mvn test
-  fi
-}
-
-localDep () {
-  for dep in ode-ts-client ode-ngjs-front ; do
-    if [ -e $PWD/../$dep ]; then
-      rm -rf $dep.tar $dep.tar.gz
-      mkdir $dep.tar && mkdir $dep.tar/dist \
-        && cp -R $PWD/../$dep/dist $PWD/../$dep/package.json $dep.tar
-      tar cfzh $dep.tar.gz $dep.tar
-      docker compose run --rm -u "$USER_UID:$GROUP_GID" $CI_OPTION node sh -c "npm install --no-save $dep.tar.gz"
-      rm -rf $dep.tar $dep.tar.gz
-    fi
-  done
-}
-
-watch () {
-  docker compose run --rm \
-    -u "$USER_UID:$GROUP_GID" $CI_OPTION \
-    -v $PWD/../$SPRINGBOARD:/home/node/$SPRINGBOARD \
-    node sh -c "npx gulp watch-$MODULE $NODE_OPTION --springboard=../$SPRINGBOARD 2>/dev/null"
-  rm -f .version.properties
-}
-
-# ex: ./build.sh -m=workspace -s=paris watch
-
-ngWatch () {
-  docker compose run --rm -u "$USER_UID:$GROUP_GID" $CI_OPTION --publish 4200:4200 node16 sh -c "npm run start"
-}
-
-infra () {
-  docker compose run --rm -u "$USER_UID:$GROUP_GID" $CI_OPTION node sh -c "npm install /home/node/infra-front"
+  docker compose run $CI_OPTION --rm maven mvn $MVN_OPTS test
 }
 
 publish() {
@@ -258,110 +139,7 @@ publish() {
   esac
 
   docker compose run --rm  maven mvn $MVN_OPTS -DrepositoryId=ode-$nexusRepository -DskipTests --settings /var/maven/.m2/settings.xml deploy
-  #cd broker-parent/broker-client/quarkus
-  #./build.sh publish
-  #cd -
-  publishBrokerNpmLib
-}
-
-publishBrokerNpmLib() {
-  # Set GIT_BRANCH to the current branch if not set
-  if [ -z "$GIT_BRANCH" ]; then
-    GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-  fi
-  echo "[publish] Publish packages..."
-  # Récupération de la branche locale
-  LOCAL_BRANCH=`echo $GIT_BRANCH | sed -e "s|origin/||g"`
-  # Récupération de la date et du timestamp
-  # Récupération du dernier tag stable
-  LATEST_TAG=$(git describe --tags --abbrev=0 2>/dev/null || echo "1.0.0")
-  LATEST_TAG=${LATEST_TAG#v}
-
-  # Définition du tag de la branche
-  if [ "$LOCAL_BRANCH" = "main" ]; then
-    TAG_BRANCH="latest"
-  else
-    TAG_BRANCH=$LOCAL_BRANCH
-  fi
-
-  # Publier avec le tag de la branche
-  echo "[publish] Publish with the branch tag"
-  # Default to dry run if not specified
-  DRY_RUN=${DRY_RUN:-true}
-  if [ "$DRY_RUN" = "true" ]; then
-    docker compose run -e NPM_TOKEN=$NPM_TOKEN --rm -u "$USER_UID:$GROUP_GID" node22 sh -c "pnpm publish -r --no-git-checks --tag $TAG_BRANCH --dry-run"
-  else
-    docker compose run -e NPM_TOKEN=$NPM_TOKEN --rm -u "$USER_UID:$GROUP_GID" node22 sh -c "pnpm publish -r --no-git-checks --tag $TAG_BRANCH"
-  fi
-}
-
-check_prefix_sh_file() {
-    dir_path=$1      # Directory path
-    search_str=$2    # String to check
-    # Loop over each .sh file in the directory
-    for file in "$dir_path"/*.sh; do
-        if [ -f "$file" ]; then
-            # Get the file name without the extension
-            base_name=$(basename "$file" .sh)
-
-            # Check if the file name is a prefix of the search string
-            if [[ "$search_str" == "$base_name"* ]]; then
-                return 0  # Found a match, return 0 (success)
-            fi
-        fi
-    done
-
-    return 1  # No match found, return 1 (failure)
-}
-
-itTests() {
-  script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-  cd $script_dir/tests/src/test/js
-  docker compose run --rm -T node18 pnpm i --force
-  cd $script_dir/tests/src/test/js/it/scenarios
-  failed_files=()
-  exit_code=0
-  js_files=($(find . -type f -name '*.ts' ! -name '_*'))
-  for it_file in "${js_files[@]}"; do
-    short_file_name=$(basename -s .ts $it_file)
-    file_dir=$(dirname $it_file)
-    check_prefix_sh_file "$file_dir" "$short_file_name"
-    if [ $? -eq 1 ]; then
-      echo executing $it_file
-      docker compose run --rm -T k6 run --compatibility-mode=experimental_enhanced file:///home/k6/src/it/scenarios/$it_file
-      if [ $? -ne 0 ]; then
-          exit_code=1
-          failed_files+=("$it_file")
-          echo "Error while executing : $it_file"
-      fi
-    fi
-  done
-  sh_files=($(find . -type f -name '*.sh'))
-  for sh_file in "${sh_files[@]}"; do
-    echo executing $sh_file
-    "$sh_file" "$script_dir/tests/src/test/resources/data" "$script_dir/../$SPRINGBOARD"
-    if [ $? -ne 0 ]; then
-        exit_code=1
-        failed_files+=("$sh_file")
-        echo "Error while executing : $sh_file"
-    fi
-  done
-  cd -
-
-  # Output summary of failed files
-  if [ ${#failed_files[@]} -ne 0 ]; then
-    echo "|-------------------------|"
-    echo "|--- FAILED TEST FILES ---|"
-    for failed in "${failed_files[@]}"; do
-      echo "| $failed"
-    done
-    echo "|-------------------------|"
-  fi
-  
-  echo "|-------------------------|"
-  [ $exit_code -ne 0 ] && echo "|---- itTests  FAILED ----|" || echo "|--- itTests SUCCEEDED ---|"
-  echo "|-------------------------|"
-  exit $exit_code
+  ./edifice publish --clients=true --dry-run=false --service=false
 }
 
 if [ ! -e .env ]; then
@@ -379,38 +157,11 @@ do
     clean)
       clean
       ;;
-    buildFrontend)
-      buildFrontend
-      ;;
-    buildBackend)
-      buildBackend
-      ;;
-    buildMvn)
-      install
-      ;;
     install)
-      buildFrontend && buildBackend  && buildBroker
-      ;;
-    buildBack)
       install
-      ;;
-    localDep)
-      localDep
-      ;;
-    watch)
-      watch
-      ;;
-    ngWatch)
-      ngWatch
       ;;
     test)
       test
-      ;;
-    itTests)
-      itTests
-      ;;
-    infra)
-      infra
       ;;
     publish)
       publish
