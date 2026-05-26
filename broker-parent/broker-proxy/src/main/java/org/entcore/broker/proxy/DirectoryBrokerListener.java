938a5f6356db33f7ca984e0a0ed9108366633ddd
Junior Bernard <junior.bernard@opendigitaleducation.com>
2026-05-26 11:14:32 +0200

    feat(broker): PEDAGO-3737 get users from groups

--- broker-parent/broker-proxy/src/main/java/org/entcore/broker/proxy/DirectoryBrokerListener.java
+++ broker-parent/broker-proxy/src/main/java/org/entcore/broker/proxy/DirectoryBrokerListener.java
@@ -70,4 +70,12 @@ public interface DirectoryBrokerListener {
    */
   @BrokerListener(subject = "directory.users.get.byids", proxy = true)
   Future<GetUsersByIdsResponseDTO> getUsersByIds(GetUsersByIdsRequestDTO request);
+
+  /**
+   * This method retrieves all users in provided groups with basic profile information
+   * @param request The request object containing the list of group IDs to get users from
+   * @return A response object containing a list of users
+   */
+  @BrokerListener(subject = "directory.users.get.bygroupids", proxy = true)
+  Future<GetUsersFromGroupsResponseDTO> getUsersFromGroups(GetUsersFromGroupsRequestDTO request);
 }
